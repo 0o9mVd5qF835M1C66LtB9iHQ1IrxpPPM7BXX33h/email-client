45dd3bb9f9371d833fed81415edb2420d24a02f5
Shane Howe <shanehowe100@hotmail.com>
2022-10-25 21:56:30 +0100

    gitignore updated and code formatting

--- imap.py
+++ imap.py
@@ -1,22 +1,28 @@
 from imaplib import IMAP4_SSL
 import email
-from smtp import User
+from smtp import User, get_password, KEY
+
+
+# TODO: Figure out how to deal with files
 
 
 class EmailReader(IMAP4_SSL):
-    def __init__(self, host, port_number, ignore_list=None) -> None:
+    ignore_list = [
+        '"The NordVPN team" <support@nordvpn.com>',
+        "LinkedIn <messages-noreply@linkedin.com>",
+    ]
+
+    def __init__(self, host, port_number) -> None:
         super().__init__(host, port_number)
         self.host = host
         self.port = port_number
-        if ignore_list is None:
-            self.ignore_list = []
-    
+
     def log_in(self, user: User) -> None:
         """
         Logs in to the server
         """
         self.login(user.email, user.password)
-    
+
     def get_emails(self, folder: str) -> list:
         """
         Gets all the emails from the specified folder
@@ -24,46 +30,36 @@ class EmailReader(IMAP4_SSL):
         emails = []
         self.select(folder)
         _, msg_nums = self.search(None, "ALL")
-
-        for num in reversed(msg_nums[0].split()):
+        revered_mail = sorted(msg_nums[0].split(), key=int, reverse=True)
+        for num in revered_mail[0:10]:
             mail = {}
             _, data = self.fetch(num, "(RFC822)")
             message = email.message_from_bytes(data[0][1])
 
             if message.get("From") in self.ignore_list:
                 continue
-            
+
             for part in message.walk():
                 if part.get_content_type() == "text/plain":
-                    body = part.get_payload(decode=True).decode("utf-8")
-                elif part.get_content_type() == "text/html":
-                   body = part.get_payload(decode=True).decode("utf-8")
-                elif part.get_content_type() == "multipart/alternative":
-                    continue
-                else:
-                    mail["attachment"] = part.get_payload(decode=True)
-                    mail["attachment_name"] = part.get_filename()
+                    body = part.as_string()
 
-            mail.update({
-                "message number": num,
-                "subject": message.get("Subject"),
-                "from": message.get("from"),
-                "to": message.get("to"),
-                "date": message.get("date"),
-                "BCC": message.get("BCC"),
-                "body": body
-            })
-
+            mail.update(
+                {
+                    "message number": num,
+                    "subject": message.get("Subject"),
+                    "from": message.get("from"),
+                    "to": message.get("to"),
+                    "date": message.get("date"),
+                    "BCC": message.get("BCC"),
+                    "body": body,
+                }
+            )
             emails.append(mail)
         return emails
-    
+
     def add_to_ignore_list(self, email_address) -> None:
         """
         Adds an email to the ignore list
+        will create a file to write the emails to
         """
-        self.ignore_list.append(email_address)
-
-
-
-        
-
+        ...
