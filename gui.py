e4980fa526a243b93515f84231a8d1fe60412044
Shane Howe <shanehowe100@hotmail.com>
2022-10-30 13:22:52 +0000

    Added functionality to read emails & created decision frame

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ from smtp import (
     get_password,
     CONFIG,
 )
+from imap import EmailReader
 
 
 class LogInFrame:
@@ -47,12 +48,12 @@ class LogInFrame:
 
         if password == password_check and username == email_check:
             self.frame.destroy()
-            MainFrame(self.master)
+            DecisionsFrame(self.master)
         else:
             PopUp(self.master, "Incorrect username or password")
 
 
-class MainFrame:
+class ComposeEmailFrame:
     def __init__(self, master: Any) -> None:
         self.master = master
         self.frame = Frame(self.master, width=800, height=800, padx=40, pady=40)
@@ -87,6 +88,9 @@ class MainFrame:
         self.log_out_button = Button(
             self.frame, text="Log Out", command=self.log_out
         ).grid(row=4, column=0, columnspan=2, pady=10)
+        self.back_button = Button(
+            self.frame, text="Back", command=self.back
+        ).grid(row=5, column=0, columnspan=2, pady=10)
 
     def send_email(self):
         to_addr = self.to_address.get()
@@ -121,6 +125,10 @@ class MainFrame:
     def log_out(self):
         self.frame.destroy()
         LogInFrame(self.master)
+    
+    def back(self):
+        self.frame.destroy()
+        DecisionsFrame(self.master)
 
 
 class PopUp:
@@ -147,6 +155,93 @@ class PopUp:
         ).pack(pady=10)
 
 
+class MailReadingFrame:
+    def __init__(self, master: Any) -> None:
+        self.master = master
+        self.frame = Frame(self.master, width=800, height=800, padx=40, pady=40)
+        self.frame.grid()
+        self.emails = imap.get_emails("INBOX")
+        self.emails_list = Listbox(self.frame, width=100, height=20)
+        self.emails_list.grid(row=0, column=0, columnspan=2)
+        self.read_button = Button(
+            self.frame, text="Read", command=self.read_email
+        ).grid(row=1, column=0, pady=10)
+        self.back_button = Button(self.frame, text="Back", command=self.back).grid(
+            row=1, column=1, pady=10
+        )
+        self.populate_list()
+
+    def populate_list(self):
+        for email in self.emails:
+            list_item = f"{email['from']} - {email['subject']}"
+            self.emails_list.insert(END, list_item)
+
+    def read_email(self):
+        selection = self.emails_list.curselection()
+        if selection:
+            email = self.emails[selection[0]]
+            self.frame.destroy()
+            EmailFrame(self.master, email)
+
+    def back(self):
+        self.frame.destroy()
+        DecisionsFrame(self.master)
+
+
+class EmailFrame:
+    def __init__(self, master: Any, email) -> None:
+        self.master = master
+        self.frame = Frame(self.master, width=800, height=800, padx=40, pady=40)
+        self.frame.grid()
+        self.email = email
+        self.email_text = Text(self.frame, width=100, height=20)
+        self.email_text.grid(row=0, column=0, columnspan=2)
+        self.back_button = Button(self.frame, text="Back", command=self.back).grid(
+            row=1, column=0, pady=10
+        )
+        self.populate_text()
+
+    def populate_text(self):
+        from_addr = self.email.get("from")
+        subject = self.email.get("subject")
+        body = self.email.get("body")
+        email_text = f"From: {from_addr}\nSubject: {subject}\n\n{body}"
+        self.email_text.insert(END, email_text)
+
+    def back(self):
+        self.frame.destroy()
+        MailReadingFrame(self.master)
+
+
+class DecisionsFrame:
+    # Frame for user to decide whether to read or compose emails
+    def __init__(self, master: Any) -> None:
+        self.master = master
+        self.frame = Frame(self.master, width=800, height=800, padx=40, pady=40)
+        self.frame.grid()
+        self.read_button = Button(
+            self.frame, text="Read Emails", command=self.read_emails
+        ).grid(row=0, column=0, pady=10)
+        self.compose_button = Button(
+            self.frame, text="Compose Email", command=self.compose_email
+        ).grid(row=0, column=1, pady=10)
+        self.log_out_button = Button(
+            self.frame, text="Log Out", command=self.log_out
+        ).grid(row=1, column=0, columnspan=2, pady=10)
+    
+    def read_emails(self):
+        self.frame.destroy()
+        MailReadingFrame(self.master)
+    
+    def compose_email(self):
+        self.frame.destroy()
+        ComposeEmailFrame(self.master)
+    
+    def log_out(self):
+        self.frame.destroy()
+        LogInFrame(self.master)
+    
+
 class App:
     def __init__(self, master: Any) -> None:
         self.master = master
@@ -155,6 +250,9 @@ class App:
 
 
 if __name__ == "__main__":
+    imap = EmailReader(CONFIG["IMAP_SERVER"], CONFIG["IMAP_PORT"])
+    imap.log_in(User(CONFIG["EMAIL"], get_password("credentials.txt", CONFIG["KEY"])))
+    imap.ignore_list_setter()
     root = Tk()
     root.title("Email Client")
     root.resizable(False, False)
